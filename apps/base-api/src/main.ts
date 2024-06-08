25b3d7258f888bc4524b78589c897b7d1040c492
0xdeafdead <raulalcantara96@gmail.com>
2024-06-07 22:44:53 -0600

    feat: added initial swagger implementation

--- apps/base-api/src/main.ts
+++ apps/base-api/src/main.ts
@@ -5,15 +5,27 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TCGStore API')
+    .setDescription('This is the official TCGStore API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const port = process.env.PORT || 3000;
+
+  SwaggerModule.setup('api', app, document);
+
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
