f3ad10c9895468d1009579988787c1d193519b87
deafdead <raulalcantara96@gmail.com>
2024-07-23 19:54:47 -0500

    fix: added new script to package to generate prisma

--- apps/base-api/src/items/item.module.ts
+++ apps/base-api/src/items/item.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
 import { ItemRepository } from './item.repository';
+import { PrismaModule } from '../prisma-service/prisma.module';
 
 @Module({
   providers: [ItemService, ItemRepository],
   controllers: [ItemController],
+  imports:[PrismaModule]
 })
 export class ItemModule {}
