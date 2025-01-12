fa9b57ea8d0b401c2cd3b347f92e8995152ead1c
0xdeafdead <raulalcantara96@gmail.com>
2025-01-12 01:34:54 -0600

    fix: commented out jest config from base-api

--- libs/jwt/jest.config.ts
+++ libs/jwt/jest.config.ts
@@ -1,3 +1,11 @@
+// import { getJestProjectsAsync } from '@nx/jest';
+
+// export default async () => ({
+//   projects: await getJestProjectsAsync(),
+// });
+// import type { Config } from 'jest';
+
+// export default async (): Promise<Config> => {
 export default {
   displayName: 'auth',
   preset: '../../jest.preset.js',
