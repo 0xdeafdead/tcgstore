cb38928d9530bb88d0cf7f8a042a2f1122454219
0xdeafdead <raulalcantara96@gmail.com>
2025-01-11 23:52:05 -0600

    fix: reworked jest config

--- libs/jwt/jest.config.ts
+++ libs/jwt/jest.config.ts
@@ -14,5 +14,15 @@ import { getJestProjectsAsync } from '@nx/jest';
 //   coverageReporters: ['lcov'],
 // };
 export default async () => ({
-  projects: [...(await getJestProjectsAsync())],
+  displayName: 'auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/auth',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
+  coverageReporters: ['lcov'],
 });
