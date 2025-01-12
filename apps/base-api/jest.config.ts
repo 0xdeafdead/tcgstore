51e54ce4bc31607ab8d8e346ecab19130954e401
0xdeafdead <raulalcantara96@gmail.com>
2025-01-11 18:55:43 -0600

    fix: restaured jest.config file for base-api

--- apps/base-api/jest.config.ts
+++ apps/base-api/jest.config.ts
@@ -1,26 +1,25 @@
 /* eslint-disable */
-import { getJestProjectsAsync } from '@nx/jest';
+// import { getJestProjectsAsync } from '@nx/jest';
 
-// export default async ()=> ({
-//   displayName: 'base-api',
-//   preset: '../../jest.preset.js',
-//   testEnvironment: 'node',
-//   transform: {
-//     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-//   },
-//   moduleFileExtensions: ['ts', 'js', 'html'],
-//   collectCoverage: true,
-//   collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
-//   coverageDirectory: '../../coverage/apps/base-api',
-//   coverageReporters: ['lcov'],
-//   coveragePathIgnorePatterns: [
-//     '<rootDir>/src/guards',
-//     '<rootDir>/src/.*.repository.*',
-//     '<rootDir>/node_modules/',
-//     '<rootDir>/src/decorators',
-//   ],
+export default {
+  displayName: 'base-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/base-api',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
+  coverageReporters: ['lcov'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/guards',
+    '<rootDir>/src/.*.repository.*',
+    '<rootDir>/src/decorators',
+  ],
+};
+
+// export default async () => ({
+//   projects: [...(await getJestProjectsAsync())],
 // });
-
-export default async () => ({
-  projects: [...(await getJestProjectsAsync())],
-});
