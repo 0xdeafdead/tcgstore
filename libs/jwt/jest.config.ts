03e7c8b0d139609df9d757e1c0a393b682055d4a
0xdeafdead <raulalcantara96@gmail.com>
2025-01-12 01:19:34 -0600

    fix: commented out jest config from base-api

--- libs/jwt/jest.config.ts
+++ libs/jwt/jest.config.ts
@@ -1,18 +1,18 @@
 /* eslint-disable */
-import type { Config } from 'jest';
+// import type { Config } from 'jest';
 
-export default async (): Promise<Config> => {
-  return {
-    displayName: 'auth',
-    preset: '../../jest.preset.js',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-    },
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/libs/auth',
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
-    coverageReporters: ['lcov'],
-  };
+// export default async (): Promise<Config> => {
+export default {
+  displayName: 'auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/auth',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
+  coverageReporters: ['lcov'],
 };
+// };
