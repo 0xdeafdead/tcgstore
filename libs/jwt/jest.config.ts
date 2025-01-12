72c0a9e44365047fe96b4772dcaf929b17e30ec8
0xdeafdead <raulalcantara96@gmail.com>
2025-01-11 18:02:18 -0600

    fix: reverted jest config file from jwt

--- libs/jwt/jest.config.ts
+++ libs/jwt/jest.config.ts
@@ -1,18 +1,18 @@
 /* eslint-disable */
-import { getJestProjectsAsync } from '@nx/jest';
-// export default {
-//   displayName: 'auth',
-//   preset: '../../jest.preset.js',
-//   testEnvironment: 'node',
-//   transform: {
-//     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-//   },
-//   moduleFileExtensions: ['ts', 'js', 'html'],
-//   coverageDirectory: '../../coverage/libs/auth',
-//   collectCoverage: true,
-//   collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
-//   coverageReporters: ['lcov'],
-// };
+// import { getJestProjectsAsync } from '@nx/jest';
 export default async () => ({
-  projects: [...(await getJestProjectsAsync())],
+  displayName: 'auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/auth',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
+  coverageReporters: ['lcov'],
 });
+// export default async () => ({
+//   projects: [...(await getJestProjectsAsync())],
+// });
