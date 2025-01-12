eb95a0730884e060bac79630997f0ac7abbf7f10
0xdeafdead <raulalcantara96@gmail.com>
2025-01-12 01:55:01 -0600

    fix: commented out jest config from base-api

--- apps/base-api/jest.config.ts
+++ apps/base-api/jest.config.ts
@@ -1,29 +1,26 @@
-import { getJestProjectsAsync } from '@nx/jest';
+// import { getJestProjectsAsync } from '@nx/jest';
 
-export default async () => ({
-  projects: await getJestProjectsAsync(),
-});
-// import type { Config } from 'jest';
+// export default async () => ({
+//   projects: await getJestProjectsAsync(),
+// });
 
-// export default async () => {
-//   //   return {
-//   //     verbose: true,
-//   //     displayName: 'base-api',
-//   //     // preset: '../../jest.preset.js',
-//   //     // testEnvironment: 'node',
-//   //     // transform: {
-//   //     //   '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-//   //     // },
-//   //     // moduleFileExtensions: ['ts', 'js', 'html'],
-//   //     // coverageDirectory: '../../coverage/apps/base-api',
-//   //     // collectCoverage: true,
-//   //     // collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
-//   //     // coverageReporters: ['lcov'],
-//   //     // coveragePathIgnorePatterns: [
-//   //     //   '<rootDir>/src/guards',
-//   //     //   '<rootDir>/src/.*.repository.*',
-//   //     //   '<rootDir>/src/decorators',
-//   //     //   '<rootDir>/src/config',
-//   //     // ],
-//   //   };
-// };
+export default {
+  verbose: true,
+  displayName: 'base-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/base-api',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.[tj]s'],
+  coverageReporters: ['lcov'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/guards',
+    '<rootDir>/src/.*.repository.*',
+    '<rootDir>/src/decorators',
+    '<rootDir>/src/config',
+  ],
+};
